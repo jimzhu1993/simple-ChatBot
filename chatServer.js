839b13450cd97d51f2260ef34f885cabb33584ca
Zhu <zhu@umail.ucsb.edu>
2018-02-05 14:57:45 -0500

    lab1

--- chatServer.js
+++ chatServer.js
@@ -30,7 +30,7 @@ io.on('connect', function(socket) {
   var questionNum = 0; // keep count of question, used for IF condition.
   socket.on('loaded', function(){// we wait until the client has loaded and contacted us that it is ready to go.
 
-  socket.emit('answer',"Hey, Hello I am \"___*-\" a simple chat bot example."); //We start with the introduction;
+  socket.emit('answer',"Hey, Hello I am Root, a simple chat bot to mess up your mind."); //We start with the introduction;
   setTimeout(timedQuestion, 2500, socket,"What is your Name?"); // Wait a moment and respond with a question.
 
 });
@@ -51,50 +51,121 @@ function bot(data,socket,questionNum) {
 
 /// These are the main statments that make up the conversation.
   if (questionNum == 0) {
-  answer= 'Hello ' + input + ' :-)';// output response
-  waitTime =2000;
-  question = 'How old are you?';			    	// load next question
+    answer= 'Hello ' + input + '! Let\'s play a puzzle game.\n All answer are consists of numbers and uppercase letters';// output response
+    waitTime =2000;
+    question = 'Let\'s say the answer to the first tricky question is \'1\', then you type \'1\' as your result, so what is the answer to this question?';			    	// load next question
   }
   else if (questionNum == 1) {
-  answer= 'Really ' + input + ' Years old? So that means you where born in: ' + (2018-parseInt(input));// output response
-  waitTime =2000;
-  question = 'Where do you live?';			    	// load next question
+    if (input == '1') {
+        answer= 'Correct, this question is too easy to trick you.';// output response
+        waitTime = 2000;
+        question = 'Should we make it a little more tricky?';			    	// load next question
+    }else{
+        answer= 'Ops, wrong answer, You got 0 question right.';// output response
+        waitTime = 2000;
+        question = 'Let\'s say the answer to the first tricky question is \'1\', then you type \'1\' as your result, so what is the answer to this question?';			    	// load next question;
+        waitTime = 2000;
+        questionNum--;
+    }
   }
   else if (questionNum == 2) {
-  answer= ' Cool! I have never been to ' + input+'.';
-  waitTime =2000;
-  question = 'Whats your favorite Color?';			    	// load next question
+    if (input == '2'){
+      answer= 'Correct, you are not not dumb as I thought you are.';// output response
+      waitTime = 2000;
+      question = 'Now, this question is twice difficult as the sum of the previous two';			    	// load next question
+    }else{
+        answer= 'Ops, wrong answer, You got only 1 question right';// output response
+        waitTime = 2000;
+        question = 'Should we make it a little more tricky?';			    	// load next question
+        waitTime = 2000;
+        questionNum--;
+    }
   }
   else if (questionNum == 3) {
-  answer= 'Ok, ' + input+' it is.';
-  socket.emit('changeBG',input.toLowerCase());
-  waitTime = 2000;
-  question = 'Can you still read the font?';			    	// load next question
+    if (input == '6'){
+      answer= 'Correct, now I guess you are good at math.';// output response
+      waitTime = 2000;
+      question = 'LOL, do you really think I have questions all like this?\nTYU\nBNM';			    	// load next question
+    }else{
+        answer= 'Ops, wrong answer, You got 2 questions right';// output response
+        waitTime = 2000;
+        question = 'Now, this question is twice difficult as the sum of the previous two';			    	// load next question
+        waitTime = 2000;
+        questionNum--;
+    }
   }
   else if (questionNum == 4) {
-    if(input.toLowerCase()==='yes'|| input===1){
-      answer = 'Perfect!';
-      waitTime =2000;
-      question = 'Whats your favorite place?';
-    }
-    else if(input.toLowerCase()==='no'|| input===0){
-        socket.emit('changeFont','white'); /// we really should look up the inverse of what we said befor.
-        answer='How about now?'
-        question='';
-        waitTime =0;
-        questionNum--; // Here we go back in the question number this can end up in a loop
+    if (input == 'GHJ'){
+      answer= 'Correct, you are indeed really smart.';// output response
+      waitTime = 2000;
+      question = 'Now things are getiing more elusive\nU KICW TIY';			    	// load next question
     }else{
-      answer=' I did not understand you. Can you please answer with simply with yes or no.'
-      question='';
-      questionNum--;
-      waitTime =0;
+        answer= 'Ops, wrong answer, You got 3 questions right';// output response
+        waitTime = 2000;
+        question = 'LOL, do you really think I have questions all like this?\nTYU\nBNM';			    	// load next question
+        waitTime = 2000;
+        questionNum--;
+    }
+  // load next question
+  }
+  else if (questionNum == 5) {
+    if (input == 'ILOVEYOU'){
+      answer= 'Correct, you are geneius.';// output response
+      waitTime = 2000;
+      question = 'Board\nDrug Dealer\nFFFFFF';			    	// load next question
+    }else{
+        answer= 'Ops, wrong answer, You got 4 questions right';// output response
+        waitTime = 2000;
+        question = 'Now things are getiing more elusive\nU KICW TIY';			    	// load next question
+        waitTime = 2000;
+        questionNum--;
+    }
+  // load next question
+  }
+  else if (questionNum == 6) {
+    if (input == 'WHITE'){
+      answer= 'Correct, let\'s keep rolling.';// output response
+      waitTime = 2000;
+      question = 'while(1)';			    	// load next question
+    }else{
+        answer= 'Ops, wrong answer, You got 5 questions right';// output response
+        waitTime = 2000;
+        question = 'Board, Drug Dealer, FFFFFF';			    	// load next question
+        waitTime = 2000;
+        questionNum--;
+    }
+  // load next question
+  }
+  else if (questionNum == 7) {
+    if (input == 'FOREVER'){
+      answer= 'Correct, I promiss you this will not go forever.';// output response
+      waitTime = 2000;
+      question = '44277999';			    	// load next question
+    }else{
+        answer= 'Ops, wrong answer, You got 6 questions right';// output response
+        waitTime = 2000;
+        question = 'while(1)';			    	// load next question
+        waitTime = 2000;
+        questionNum--;
+    }
+  // load next question
+  }
+  else if (questionNum == 8) {
+    if (input == 'HAPPY'){
+      answer= 'Congraduation! You are the ultimate winner';// output response
+      waitTime = 2000;
+      question = 'Thank you for paticipating!'
+    }else{
+        answer= 'Ops, wrong answer, You got 7 questions right';// output response
+        waitTime = 2000;
+        question = '44277999';			    	// load next question
+        waitTime = 2000;
+        questionNum--;
     }
   // load next question
   }
   else{
-    answer= 'I have nothing more to say!';// output response
-    waitTime =0;
-    question = '';
+    answer= 'Thank you for paticipating!';// output response
   }
 
 
